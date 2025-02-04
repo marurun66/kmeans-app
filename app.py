5ee723a4c7377877252d482d066b23bd0190380b
marurun66 <marurun66@gmail.com>
2025-02-04 13:06:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ import matplotlib.pyplot as plt
 ## 한글 폰트 설정
 import os
 import matplotlib.font_manager as fm
+
+
+
 @st.cache_data
 def fontRegistered():
     font_dirs = [os.getcwd() + '/custom_fonts']
     font_files = fm.findSystemFonts(fontpaths=font_dirs)
     for font_file in font_files:
         fm.fontManager.addfont(font_file)
+    st.cache_data.clear()
     fm._load_fontmanager(try_read_cache=False)
 ## 한글폰트 설정
 
